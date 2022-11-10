d57a051c012a0de8ef0fa45fb96bc3315412300a
jmshima01 <jamesshima@mines.edu>
2022-11-10 10:58:23 -0700

    cleaned dataset to have no spaces and consistent naming

--- clean_data.py
+++ clean_data.py
@@ -2,6 +2,63 @@
 """
 Created on Thu Nov 10 09:36:20 2022
 
-@author: James
-"""
+::: 403 Database Management :::
+::: Dataset scrubing script :::
 
+@author: James Shima
+"""
+import csv
+
+unique_lines = set()
+file_path = input("Please enter dataset filepath: ")
+
+
+data = []
+final_data = []
+
+with open(file_path, 'r+', encoding='utf-8') as dataset:
+    reader = csv.reader(dataset)
+    
+    
+    # Cleaning extra spaces and weird inconsistencies:
+    for line in reader:
+        temp = []
+        start = 1
+        for string in line:
+            if (start == 1):
+                no_spaces = string.replace(' ', '_')
+                if(no_spaces[-1] == '_'):
+                    no_spaces = no_spaces[:len(no_spaces)-1]
+                no_spaces = no_spaces.replace('__', '_')
+                temp.append(no_spaces.lower())
+                start = 0
+            else:
+                no_spaces = string.replace(' ', '')
+                temp.append(no_spaces)
+            
+                
+        data.append(temp)
+        
+    
+    # Taking only unique rows:
+    for l in data:
+        if(l not in final_data):
+            final_data.append(l)
+    
+    # uncomment to see data in terminal:
+    # for i in final_data:
+    #     print(i)
+    
+
+with open("symptoms_to_disease_dataset.csv", 'w+', encoding='utf-8', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(final_data)
+    print('\n done.')
+    
+        
+    
+    
+    
+    # for i in unique_lines:
+    #     print(i)
+        
