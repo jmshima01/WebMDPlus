6a82f88e12112b78da373c176a4a57d34bef5a6d
viktorijamikelevicius <114948151+viktorijamikelevicius@users.noreply.github.com>
2022-11-21 15:10:44 -0700

    Queries w/o patient table

--- SQLfiles/queries.sql
+++ SQLfiles/queries.sql
@@ -1,1 +1,25 @@
 -- Go ahead and run interesting queries here!
+
+--Queries w/o patient table
+SELECT name, description, symptom
+FROM disease d, disease_symptoms ds
+WHERE d.name = ds.disease_name;
+
+SELECT name, description, symptom
+FROM disease d, disease_symptoms ds
+WHERE d.name = ds.disease_name
+  AND d.name ILIKE 'f%';
+
+SELECT name, COUNT(symptom)
+FROM disease d, disease_symptoms ds
+WHERE d.name = ds.disease_name
+GROUP BY d.name;
+  
+SELECT name, COUNT(symptom)
+FROM disease d, disease_symptoms ds
+WHERE d.name = ds.disease_name
+  AND d.name IN 
+  (SELECT disease_name FROM medication
+   WHERE medication_name ILIKE '%ine'
+        AND medication_name NOT ILIKE 'obsolete')
+GROUP BY d.name;
